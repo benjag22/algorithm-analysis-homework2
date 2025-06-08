200d7d468fa0170fe95bbc3da92c86ac334c6592
Pixoll <pixoll.gamer@gmail.com>
2025-06-08 17:31:57 -0400

    refactor: edit distance algorithm is symetric

--- src/main.cpp
+++ src/main.cpp
@@ -18,44 +18,42 @@ int main(const int argc, const char *const *const argv) {
     text_extractors.emplace_back(DATA_DIR + "/extract_3.txt", args.start_pos);
     text_extractors.emplace_back(DATA_DIR + "/extract_4.txt", args.start_pos);
 
-    for (int i = 0; i < text_extractors.size(); i++) {
-        for (int j = 0; j < text_extractors.size(); j++) {
-            if (i != j) {
-                TextExtractor &text_extractor1 = text_extractors[i];
-                TextExtractor &text_extractor2 = text_extractors[j];
+    for (int i = 0; i < text_extractors.size() - 1; i++) {
+        for (int j = i + 1; j < text_extractors.size(); j++) {
+            TextExtractor &text_extractor1 = text_extractors[i];
+            TextExtractor &text_extractor2 = text_extractors[j];
 
-                const std::string suffix = std::to_string(i) + "_" + std::to_string(j) + ".csv";
+            const std::string suffix = std::to_string(i) + "_" + std::to_string(j) + ".csv";
 
-                uhr<EditDistanceMemo>(
-                    DATA_DIR + "/memo_" + suffix,
-                    args.runs,
-                    args.lower,
-                    args.upper,
-                    args.step,
-                    text_extractor1,
-                    text_extractor2
-                );
+            uhr<EditDistanceMemo>(
+                DATA_DIR + "/memo_" + suffix,
+                args.runs,
+                args.lower,
+                args.upper,
+                args.step,
+                text_extractor1,
+                text_extractor2
+            );
 
-                uhr<EditDistanceDP>(
-                    DATA_DIR + "/dp_" + suffix,
-                    args.runs,
-                    args.lower,
-                    args.upper,
-                    args.step,
-                    text_extractor1,
-                    text_extractor2
-                );
+            uhr<EditDistanceDP>(
+                DATA_DIR + "/dp_" + suffix,
+                args.runs,
+                args.lower,
+                args.upper,
+                args.step,
+                text_extractor1,
+                text_extractor2
+            );
 
-                uhr<EditDistanceDPOptimized>(
-                    DATA_DIR + "/dp_optimized_" + suffix,
-                    args.runs,
-                    args.lower,
-                    args.upper,
-                    args.step,
-                    text_extractor1,
-                    text_extractor2
-                );
-            }
+            uhr<EditDistanceDPOptimized>(
+                DATA_DIR + "/dp_optimized_" + suffix,
+                args.runs,
+                args.lower,
+                args.upper,
+                args.step,
+                text_extractor1,
+                text_extractor2
+            );
         }
     }
 
